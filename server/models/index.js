61fd0d82d5b0192c3f98f63b74652f186b67bb26
CollyLee <clh5160@gmail.com>
2023-05-30 14:24:01 -0400

    updated schemas and models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Product = require('./Product');
-const Category = require('./Category');
+const Brand = require('./Brand');
 const Order = require('./Order');
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, Product, Brand, Order };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,18 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User, Product, Brand, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
-    categories: async () => {
-      return await Category.find();
+    brands: async () => {
+      return await Brand.find();
     },
-    products: async (parent, { category, name }) => {
+    products: async (parent, { brand, name }) => {
       const params = {};
 
-      if (category) {
-        params.category = category;
+      if (brand) {
+        params.brand = brand;
       }
 
       if (name) {
@@ -21,16 +21,16 @@ const resolvers = {
         };
       }
 
-      return await Product.find(params).populate('category');
+      return await Product.find(params).populate('brand');
     },
     product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
+      return await Product.findById(_id).populate('brand');
     },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
           path: 'orders.products',
-          populate: 'category'
+          populate: 'brand'
         });
 
         user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
@@ -44,7 +44,7 @@ const resolvers = {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
           path: 'orders.products',
-          populate: 'category'
+          populate: 'brand'
         });
 
         return user.orders.id(_id);
